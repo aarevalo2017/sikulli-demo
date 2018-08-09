92d7396d240a3340c4debf491faba9e276c990e1
Alejanddro Arévalo S <alejandroordoner.arevalosanchez@externos-cl.cencosud.com>
2018-08-09 16:55:42 -0400

    Se agregan images y se actualiza path.

--- src/main/java/cl/cencosud/sikulidemo/PuttyObjects.java
+++ src/main/java/cl/cencosud/sikulidemo/PuttyObjects.java
@@ -4,10 +4,10 @@ import org.sikuli.script.Pattern;
 
 public class PuttyObjects {
 
-	public static final Pattern ICONO = new Pattern("C:\\picturesPutty\\ICONO.png");
-	public static final Pattern BTN_OPEN = new Pattern("C:\\picturesPutty\\BTN_OPEN.png");
-	public static final Pattern BTN_CERRAR = new Pattern("C:\\picturesPutty\\BTN_CERRAR.png");
-	public static final Pattern OPT_TELNET = new Pattern("C:\\picturesPutty\\OPT_TELNET.png");
-	public static final Pattern BTN_ACEPTAR = new Pattern("C:\\picturesPutty\\BTN_ACEPTAR.png");
+	public static final Pattern ICONO = new Pattern(System.getProperty("user.dir") + "\\putty-pictures\\ICONO.png");
+	public static final Pattern BTN_OPEN = new Pattern(System.getProperty("user.dir") + "\\putty-pictures\\BTN_OPEN.png");
+	public static final Pattern BTN_CERRAR = new Pattern(System.getProperty("user.dir") + "\\putty-pictures\\BTN_CERRAR.png");
+	public static final Pattern OPT_TELNET = new Pattern(System.getProperty("user.dir") + "\\putty-pictures\\OPT_TELNET.png");
+	public static final Pattern BTN_ACEPTAR = new Pattern(System.getProperty("user.dir") + "\\putty-pictures\\BTN_ACEPTAR.png");
 
 }
